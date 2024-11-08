6a5f89253c76b6181c3889b2285ae65748f312f0
Evan <evan.goris@xillio.nl>
2024-11-08 11:41:04 +0100

    Add required modules. Bit of refactoring

---
+++ ImportSPO.ps1
@@ -0,0 +1,121 @@
+﻿function Trigger-Jobs
+{
+    param(
+        [Parameter(Mandatory=$True, ParameterSetName="Standard")][String]$Username,
+        [Parameter(Mandatory=$True, ParameterSetName="Standard")][String]$Password,
+        [Parameter(Mandatory=$True, ParameterSetName="Interactive")][Switch]$Interactive,
+        [Parameter(Mandatory=$True, ParameterSetName="Certificate")][String]$ClientId,
+        [Parameter(Mandatory=$True, ParameterSetName="Certificate")][String]$Thumbprint,
+        [String]$MongoHost='127.0.0.1',
+        [String]$MonogPort=27017,
+        [Parameter(Mandatory=$True)][String]$MongoDatabase,
+        [String]$MongoCollection='documents'
+    )
+
+    switch ($PSCmdlet.ParameterSetName)
+    {
+        'Standard'
+        {
+            <# Run in SharePointOnline Management Shell. Don't load PnP #>
+            Import-Module Mdbc -ErrorAction Stop
+            break
+        }
+        {$_ -eq 'Interactive' -or $_ -eq 'Certificate'}
+        {
+            <# Run in PowerShell 7. Load PnP #>
+            Import-Module PnP.PowerShell -ErrorAction Stop
+            Import-Module Mdbc -ErrorAction Stop
+            break
+        }
+        Default
+        {
+            Write-Host "Invalid Arguments" -ForegroundColor Red
+            exit
+        }
+    }
+
+    $mongocs  = "mongodb://$($MongoHost):$($MongoPort)/$($MongoDatabase)"
+    Connect-Mdbc -connectionstring $mongocs -databasename $MongoDatabase -collectionname $MongoCollection -ErrorAction Stop
+
+    $global:context = $null
+
+    Get-MdbcData -Filter @{"migration.migrate"=$True; "source.contentType.systemName"="SPOPackage"; "source.properties.azure.migrationJobId"=$null} -ErrorAction Stop |
+    ForEach {
+        try
+        {
+            $package = $_
+
+            $siteurl  = $package.source.properties.spo.webUrl
+            $dataUri  = $package.source.properties.azure.azureContainerSourceUri
+            $metaUri  = $package.source.properties.azure.azureContainerManifestUri
+            $queueUri = $package.source.properties.azure.azureQueueReportUri
+            $enckey   = [Convert]::FromBase64String($package.source.properties.azure.encryptionKey)
+
+            if($null -eq $context -or $context.url.toLower() -ne $siteurl.toLower())
+            {
+                Write-Host "Connecting to $siteurl"
+                switch ($PSCmdlet.ParameterSetName)
+                {
+                    'Standard'
+                    {
+                        $context = New-Object Microsoft.SharePoint.Client.ClientContext($siteurl)    
+                        $context.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, (ConvertTo-SecureString -AsPlainText -Force -String $password))
+                        break
+                    }
+                    'Interactive'
+                    {
+                        $pnpconnection = Connect-PnPOnline -Interactive -Url $siteurl -ReturnConnection -WarningAction Ignore -ClientId $clientid -ErrorAction Stop
+                        $context = $pnpconnection.context
+                        break
+                    }
+                    'Certificate'
+                    {
+                        $pnpconnection = Connect-PnPOnline -Url $siteurl -ReturnConnection -WarningAction Ignore -ClientId $ClientId -Thumprint $Thumbprint -ErrorAction Stop
+                        $context = $pnpconnection.context
+                        break
+                    }
+                    Default
+                    {
+                        Write-Host "Invalid Arguments" -ForegroundColor Red
+                        exit
+                    }
+                }
+
+                while($True)
+                {
+                    try
+                    {
+                        $context.load($context.web)
+                        $context.executequery()
+                        break
+                    }
+                    catch
+                    {
+                        if($_.Exception.Message.contains("The underlying connection was closed"))
+                        {
+                            Write-Host $_.Exception.Message -ForegroundColor Red
+                            Write-Host "Retrying..."
+                            Start-Sleep -Seconds 5
+                        }
+                        else
+                        {
+                            throw
+                        }
+                    }
+                }
+            }
+
+            $private:enc = new-object microsoft.sharepoint.client.encryptionoption
+            $enc.aes256cbckey = $enckey
+            $jobid = $context.site.CreateMigrationJobEncrypted($context.web.id, $dataUri, $metaUri, $queueUri, $enc)
+            $context.executequery()
+            Write-Host "Job Invoked $($jobid.value.toString())"
+            Update-MdbcData -Filter @{'_id'=$package._id} -Update @{'$set'=@{'source.properties.azure.migrationJobId'=$jobid.value.toString(); 'migration.failed'=$False}}
+        }
+        catch
+        {
+            Write-Host $_.Exception.Message -ForegroundColor Red
+            Update-MdbcData -Filter @{'_id'=$package._id} -Update @{'$set'=@{'migration.failed'=$True; 'migration.failedMessage'=$_.Exception.Message}}
+        }
+    }
+}
--- TriggerJobs.ps1
+++ TriggerJobs.ps1
@@ -1,152 +1,25 @@
-﻿param(
-    [Parameter(ParameterSetName="Standard")][String]$username,
-    [Parameter(ParameterSetName="Standard")][String]$password,
-    [Parameter(ParameterSetName="Interactive")][Switch]$Interactive,
-    [Parameter(ParameterSetName="Certificate")][Switch]$Certificate
-)
+﻿. .\ImportSPO.ps1
+
 <#
     CONFIG
-
-    Register PnP by doing:
-     Register-PnPEntraIDApp -ApplicationName "PnP PowerShell App Registration" -Tenant [tenant].onmicrosoft.com -Interactive
-
-    This will also generate a self-signed certificate, uploads it to Azure, and stores a pfx file in the current directory
-    Store the certificate in the local store by dubble-clicking the pfx file
-    Get the thumbprint and the clientid from the Azure portal
-    Connect to SharePoint via Connect-PnPOnline -ClientId CLIENTID -Thumbprint THUMBPRINT -Tenant TENANT -Url URL
-
 #>
-$clientid = '62b0270a-153a-4fb2-b8b9-602fb1131362'
-$thumbprint = ''
+$MongoHost       = '127.0.0.1'
+$MongoPort       = 27017
+$MongoDatabase   = 'NAME'
+$mongoCollection = 'documents'
 
-$database = "otcs_extraction_050724"
-$mongocs  = "mongodb://HOST:27017/$($database)"
+$ClientId   = 'CLIENTID'
+$Thumbprint = 'THUMBPRINT'
 
 <#
-    START
+    Make sure Mdbc and PnP.PowerShell are in the module path
 #>
-switch ($PSCmdlet.ParameterSetName)
+if(-not $env.PSModulePath.contains('.\Modules'))
 {
-    'Standard'
-    {
-        <# Run in SharePointOnline Management Shell. Don't load PnP #>
-        try
-        {
-            Import-Module Mdbc -ErrorAction Stop
-        }
-        catch
-        {
-            $env:PSModulePath = $env:PSModulePath+";.\Modules"
-            Import-Module Mdbc -ErrorAction Stop
-        }
-        break
-    }
-    {$_ -eq 'Interactive' -or $_ -eq 'Certificate'}
-    {
-        <# Run in PowerShell 7. Load PnP #>
-        try
-        {
-            Import-Module PnP.PowerShell -ErrorAction Stop
-            Import-Module Mdbc -ErrorAction Stop
-        }
-        catch
-        {
-            $env:PSModulePath = $env:PSModulePath+";.\Modules"
-            Import-Module PnP.PowerShell -ErrorAction Stop
-            Import-Module Mdbc -ErrorAction Stop
-        }
-        break
-    }
-    Default
-    {
-        Write-Host "Invalid Arguments" -ForegroundColor Red
-        exit
-    }
+    $env:PSModulePath = $env:PSModulePath+";.\Modules"
 }
 
-Connect-Mdbc -connectionstring $mongocs -databasename $database -collectionname 'documents' -ErrorAction Stop
-
-$global:context = $null
-
-Get-MdbcData -Filter @{"migration.migrate"=$True; "source.contentType.systemName"="SPOPackage"; "source.properties.azure.migrationJobId"=$null} |
-ForEach {
-    try
-    {
-        $package = $_
-
-        $siteurl  = $package.source.properties.spo.webUrl
-        $dataUri  = $package.source.properties.azure.azureContainerSourceUri
-        $metaUri  = $package.source.properties.azure.azureContainerManifestUri
-        $queueUri = $package.source.properties.azure.azureQueueReportUri
-        $enckey   = [Convert]::FromBase64String($package.source.properties.azure.encryptionKey)
-
-        if($null -eq $context -or $context.url.toLower() -ne $siteurl.toLower())
-        {
-            Write-Host "Connecting to $siteurl"
-            switch ($PSCmdlet.ParameterSetName)
-            {
-                'Standard'
-                {
-                    $credentials = new-object System.Management.Automation.PSCredential($username, (ConvertTo-SecureString -AsPlainText -Force -String $password))
-
-                    $context = New-Object Microsoft.SharePoint.Client.ClientContext($siteurl)    
-                    $context.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, (ConvertTo-SecureString -AsPlainText -Force -String $password))
-                    #$pnpconnection = Connect-PnPOnline -Credentials $credentials -Url $siteurl -ReturnConnection -WarningAction Ignore -ClientId $clientid -ErrorAction Stop
-                    break
-                }
-                'Interactive'
-                {
-                    $pnpconnection = Connect-PnPOnline -Interactive -Url $siteurl -ReturnConnection -WarningAction Ignore -ClientId $clientid -ErrorAction Stop
-                    $context = $pnpconnection.context
-                    break
-                }
-                'Certificate'
-                {
-                    $pnpconnection = Connect-PnPOnline -Url $siteurl -ReturnConnection -WarningAction Ignore -ClientId $clientid -Thumprint $thumbprint -ErrorAction Stop
-                    $context = $pnpconnection.context
-                    break
-                }
-                Default
-                {
-                    Write-Host "Invalid Arguments" -ForegroundColor Red
-                    exit
-                }
-            }
-
-            while($True)
-            {
-                try
-                {
-                    $context.load($context.web)
-                    $context.executequery()
-                    break
-                }
-                catch
-                {
-                    if($_.Exception.Message.contains("The underlying connection was closed"))
-                    {
-                        Write-Host $_.Exception.Message -ForegroundColor Red
-                        Write-Host "Retrying..."
-                        Start-Sleep -Seconds 5
-                    }
-                    else
-                    {
-                        throw
-                    }
-                }
-            }
-        }
-
-        $private:enc = new-object microsoft.sharepoint.client.encryptionoption
-        $enc.aes256cbckey = $enckey
-        $jobid = $context.site.CreateMigrationJobEncrypted($context.web.id, $dataUri, $metaUri, $queueUri, $enc)
-        $context.executequery()
-        Write-Host "Job Invoked $($jobid.value.toString())"
-        Update-MdbcData -Filter @{'_id'=$package._id} -Update @{'$set'=@{'source.properties.azure.migrationJobId'=$jobid.value.toString(); 'migration.failed'=$False}}
-    }
-    catch
-    {
-        Write-Host $_.Exception.Message -ForegroundColor Red
-        Update-MdbcData -Filter @{'_id'=$package._id} -Update @{'$set'=@{'migration.failed'=$True; 'migration.failedMessage'=$_.Exception.Message}}
-    }
-}
+<#
+    RUN
+#>
+Trigget-Jobs -MongoDatabase $MongoDatabase -MongoHost $MongoHost -MongoPort $MongoPort -MongoColleciton $MongoCollection -ClientId $ClientId -Thumprint $Thumbprint
